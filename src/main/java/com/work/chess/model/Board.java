e3fb546a310defad5e7ab20cbcfb1532e9bd7f35
Smeltier <connect.goncalves@gmail.com>
2025-12-01 14:01:37 -0300

    att: atualiza import na classe Board.

--- src/main/java/com/work/chess/model/Board.java
+++ src/main/java/com/work/chess/model/Board.java
@@ -1,7 +1,5 @@
 package main.java.com.work.chess.model;
 
-import main.java.com.work.chess.model.Piece;;
-
 public class Board {
     private final int BOARDSIZE = 8;
     private Piece[][] board = new Piece[BOARDSIZE][BOARDSIZE];
