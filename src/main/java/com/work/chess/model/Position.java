4293cf476832aafc903255fcea9333d07fd3dc0a
Smeltier <connect.goncalves@gmail.com>
2025-12-01 13:58:53 -0300

    add: adiciona a data class Position, responsável por representar uma posição baseada em (row, col).

---
+++ src/main/java/com/work/chess/model/Position.java
@@ -0,0 +1,19 @@
+package main.java.com.work.chess.model;
+
+public class Position {
+    private final int row;
+    private final int col;
+
+    public Position (int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+}
