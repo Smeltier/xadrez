95d6dfcc83f534d2ada8aff0f12b6b133733784f
Smeltier <connect.goncalves@gmail.com>
2025-12-04 19:35:45 -0300

    att:

--- src/main/java/com/work/chess/Main.java
+++ src/main/java/com/work/chess/Main.java
@@ -2,14 +2,21 @@ package main.java.com.work.chess;
 
 import main.java.com.work.chess.view.BoardPrinter;
 import main.java.com.work.chess.model.Board;
+import main.java.com.work.chess.model.Pawn;
+import main.java.com.work.chess.model.Position;
 import main.java.com.work.chess.util.ConsoleUtils;
+import main.java.com.work.chess.util.PieceColor;
 
 public class Main {
     public static void main (String[] args) {
-        // Game game = new Game();
-        // game.run();    
-
         Board board = new Board();
+
+        Pawn pawn = new Pawn("♘", PieceColor.BLACK, new Position(1, 1));
+
+        try {
+            board.setPieceAt(pawn.getPosition(), pawn);
+        } catch (Exception e) {}
+
         ConsoleUtils.clearScreen();
         BoardPrinter.print(board);
     }
--- src/main/java/com/work/chess/model/Board.java
+++ src/main/java/com/work/chess/model/Board.java
@@ -1,17 +1,27 @@
 package main.java.com.work.chess.model;
 
+import java.security.InvalidParameterException;
+
 public class Board {
     private final int BOARDSIZE = 8;
     private Piece[][] board = new Piece[BOARDSIZE][BOARDSIZE];
 
-    public Board () {
-    }
+    public Board () {}
 
     public Piece getPieceAt (Position position) {
-        return board[position.getRow()][position.getCol()];
+        return board[position.getRow() - 1][position.getCol() - 1];
     }
 
-    public void setPieceAt (Position position, Piece piece) {
+    public void setPieceAt (Position position, Piece piece) throws Exception {
+        if (!isOnLimits(position)) {
+            throw new InvalidParameterException("Posição fora dos limites do tabuleiro.");
+        }
         board[position.getRow()][position.getCol()] = piece;
+    }
+    
+    private boolean isOnLimits (Position position) {
+        int row = position.getRow();
+        int col = position.getCol();
+        return row >= 0 && row < this.BOARDSIZE && col >= 0 && row < this.BOARDSIZE;
     }
 }
