1432a595e1417aeb0087a72d7444786e403c42fb
Smeltier <connect.goncalves@gmail.com>
2025-12-04 19:31:18 -0300

    att: trata exceções no método move.

--- src/main/java/com/work/chess/controller/PieceMover.java
+++ src/main/java/com/work/chess/controller/PieceMover.java
@@ -1,6 +1,8 @@
 package main.java.com.work.chess.controller;
 
 import main.java.com.work.chess.model.Piece;
+import java.security.InvalidParameterException;
+
 import main.java.com.work.chess.model.Board;
 import main.java.com.work.chess.model.Position;
 
@@ -10,9 +12,13 @@ public class PieceMover {
     public static void move (Board board, Piece piece, Position newPosition) {
         PieceMover.validateMove(piece, board, newPosition);
 
-        board.setPieceAt(piece.getPosition(), null);
-        piece.setPosition(newPosition);
-        board.setPieceAt(newPosition, piece);
+        try {
+            board.setPieceAt(piece.getPosition(), null);
+            piece.setPosition(newPosition);
+            board.setPieceAt(newPosition, piece);
+        } catch (Exception e) {
+            throw new InvalidParameterException("Posição inválida.");
+        }
     }
 
     private static void validateMove (Piece piece, Board board, Position newPosition) {
