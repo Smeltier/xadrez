18fcbd46b8e4e011c12612ca6f23e4db876c096f
Joao Lucas <joaolucaspbwc@gmail.com>
2025-12-06 16:23:29 -0300

    menu

--- src/main/java/com/work/chess/model/King.java
+++ src/main/java/com/work/chess/model/King.java
@@ -1,5 +1,16 @@
 
-public class King{ 
-    
+package main.java.com.work.chess.model;
+ import main.java.com.work.chess.model.Piece;
+ import java.util.List;
 
-}
+ public class King extends Piece{ 
+    
+   public List <Position> getValidMoves (Board board){
+
+     
+
+   }
+ 
+
+
+ }
---
+++ src/main/java/com/work/chess/view/Menu.java
@@ -0,0 +1,30 @@
+package main.java.com.work.chess.view;
+
+public class Menu {
+    
+    public static void menuPrincipal(String[] args) {
+
+        System.out.println("=================================");
+
+        System.out.println(" BEM - VINDO AO JOGO XADREZ");
+
+        System.out.println("=================================");
+        
+        System.out.print("Escolha uma opção: ");
+        System.out.println("1 - Iniciar nova partida");
+        System.out.println("2 - Carregar partida");
+        System.out.println("3 - Regras do jogo");
+        System.out.println("4 - Sair");
+
+        System.out.println("=================================");
+         System.out.println("ESCOLHA UMA OPÇÃO PARA A PROMOÇÃO DO PEÃO")
+         System.out.println("1-Dama");
+         System.out.println("2-Torre");
+         System.out.println("3-Cavalo");
+         System.out.println("4-Bispo");
+       
+    }
+
+   
+
+}
