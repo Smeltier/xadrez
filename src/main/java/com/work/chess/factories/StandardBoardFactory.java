a71a280513ef6b11a0eb1c3005ad5bda2404afea
Smeltier <connect.goncalves@gmail.com>
2025-12-08 22:19:02 -0300

    add: cria algumas fábricas úteis no projeto.

---
+++ src/main/java/com/work/chess/factories/StandardBoardFactory.java
@@ -0,0 +1,38 @@
+package main.java.com.work.chess.factories;
+
+import main.java.com.work.chess.enums.PieceColor;
+import main.java.com.work.chess.model.Board;
+import main.java.com.work.chess.model.Position;
+import main.java.com.work.chess.model.chess_pieces.*;
+
+public class StandardBoardFactory {
+
+    public static Board createBoard () {
+        Board board = new Board ();
+        setupSide (board, PieceColor.WHITE);
+        setupSide (board, PieceColor.BLACK);
+        return board;
+    }
+
+    private static void setupSide (Board board, PieceColor color) {
+        int mainRow = (color == PieceColor.WHITE) ? 1 : 8;
+        int pawnRow = (color == PieceColor.WHITE) ? 2 : 7;
+
+        try {
+            board.setPieceAt (new Position (mainRow, 1), new Rook (color, new Position (mainRow, 1)));
+            board.setPieceAt (new Position (mainRow, 2), new Knight (color, new Position (mainRow, 2)));
+            board.setPieceAt (new Position (mainRow, 3), new Bishop (color, new Position (mainRow, 3)));
+            board.setPieceAt (new Position (mainRow, 4), new Queen (color, new Position (mainRow, 4)));
+            board.setPieceAt (new Position (mainRow, 5), new King (color, new Position (mainRow, 5)));
+            board.setPieceAt (new Position (mainRow, 6), new Bishop (color, new Position (mainRow, 6)));
+            board.setPieceAt (new Position (mainRow, 7), new Knight (color, new Position (mainRow, 7)));
+            board.setPieceAt (new Position (mainRow, 8), new Rook (color, new Position (mainRow, 8)));
+
+            for (int i = 1; i <= 8; i++) {
+                board.setPieceAt (new Position (pawnRow, i), new Pawn (color, new Position (pawnRow, i)));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException (e.getMessage ());
+        }
+    }
+}
---
+++ src/main/java/com/work/chess/factories/SymbolFactory.java
@@ -0,0 +1,30 @@
+package main.java.com.work.chess.factories;
+
+import main.java.com.work.chess.enums.PieceType;
+import main.java.com.work.chess.enums.PieceColor;
+
+import java.util.Map;
+
+public class SymbolFactory {
+
+    private static final Map<String, String> SYMBOLS = Map.ofEntries(
+        Map.entry("QUEEN_WHITE", "♕"),
+        Map.entry("QUEEN_BLACK", "♛"),
+        Map.entry("KING_WHITE", "♔"),
+        Map.entry("KING_BLACK", "♚"),
+        Map.entry("ROOK_WHITE", "♖"),
+        Map.entry("ROOK_BLACK", "♜"),
+        Map.entry("BISHOP_WHITE", "♗"),
+        Map.entry("BISHOP_BLACK", "♝"),
+        Map.entry("KNIGHT_WHITE", "♘"),
+        Map.entry("KNIGHT_BLACK", "♞"),
+        Map.entry("PAWN_WHITE", "♙"),
+        Map.entry("PAWN_BLACK", "♟")
+    );
+
+    private SymbolFactory() {}
+
+    public static String getSymbol(PieceType type, PieceColor color) {
+        return SYMBOLS.get(type.name() + "_" + color.name());
+    }
+}
