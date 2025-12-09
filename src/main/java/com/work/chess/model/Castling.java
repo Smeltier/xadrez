d47af8cd0b8cc3a26c488d26851dc1c6a895c969
Smeltier <connect.goncalves@gmail.com>
2025-12-08 22:16:35 -0300

    add: implementa o movimento especial Roque (Castling).

---
+++ src/main/java/com/work/chess/model/Castling.java
@@ -0,0 +1,71 @@
+package main.java.com.work.chess.model;
+
+import main.java.com.work.chess.interfaces.ISpecialMoves;
+import main.java.com.work.chess.model.chess_pieces.King;
+import main.java.com.work.chess.model.chess_pieces.Piece;
+import main.java.com.work.chess.model.chess_pieces.Rook;
+
+public class Castling implements ISpecialMoves {
+
+    @Override
+    public boolean canExecute (Board board, Move move) {
+        if (!(move.piece instanceof King)) {
+            return false;
+        }
+
+        int deltaCol = Math.abs (move.from.getCol () - move.to.getCol ());
+        if (deltaCol != 2) return false;
+
+        if (!move.piece.isFirstMove ()) return false;
+
+        int row = move.from.getRow ();
+        boolean isKingside = move.to.getCol () > move.from.getCol ();
+        int rookCol = isKingside ? 8 : 1;
+        
+        Piece rook = board.getPieceAt (new Position (row, rookCol));
+        
+        if (rook == null || !(rook instanceof Rook) || !rook.isFirstMove ()) {
+            return false;
+        }
+
+        int startCol = Math.min (move.from.getCol (), rookCol) + 1;
+        int endCol = Math.max (move.from.getCol (), rookCol) - 1;
+
+        for (int col = startCol; col <= endCol; col++) {
+            if (board.getPieceAt (new Position (row, col)) != null) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public void execute (Board board, Move move) {
+        int row = move.to.getRow ();
+        boolean isKingside = move.to.getCol () > move.from.getCol ();
+
+        Position rookOriginPos;
+        Position rookTargetPos;
+
+        if (isKingside) {
+            rookOriginPos = new Position (row, 8);
+            rookTargetPos = new Position (row, 6);
+        } else {
+            rookOriginPos = new Position (row, 1);
+            rookTargetPos = new Position (row, 4);
+        }
+
+        Piece rook = board.getPieceAt (rookOriginPos);
+        
+        if (rook != null) {
+            try {
+                board.setPieceAt (rookOriginPos, null);
+                rook.setPosition (rookTargetPos);
+                board.setPieceAt (rookTargetPos, rook);
+            } catch (Exception e) {
+                System.out.println (e.getMessage ());
+            }
+        }
+    }
+}
