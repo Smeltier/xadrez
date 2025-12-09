564b65fa342749493dc1e7c0ded68a0fc1d6dead
Smeltier <connect.goncalves@gmail.com>
2025-12-08 22:17:43 -0300

    att: implementa métodos base de comparação e visual de Position.

--- src/main/java/com/work/chess/model/Position.java
+++ src/main/java/com/work/chess/model/Position.java
@@ -1,28 +1,41 @@
 package main.java.com.work.chess.model;
 
+import java.util.Objects; // Necessário para hashCode
+
 public class Position {
     private final int row;
     private final int col;
 
-    /**
-      * Cria a representação de uma posição.
-      * 
-      */
     public Position (int row, int col) {
         this.row = row;
         this.col = col;
     }
 
-    public int getCol() {
-        return col;
-    }
-
-    public int getRow() {
-        return row;
+    @Override
+    public boolean equals (Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        
+        Position other = (Position) obj;
+        return row == other.row && col == other.col;
     }
 
     @Override
-    public boolean equals (Object obj) {
-        return true;
+    public int hashCode () {
+        return Objects.hash(row, col);
+    }
+    
+    @Override
+    public String toString () {
+        char file = (char) ('A' + col - 1); 
+        return "" + file + row;
+    }
+
+    public int getCol () {
+        return col;
+    }
+
+    public int getRow () {
+        return row;
     }
 }
