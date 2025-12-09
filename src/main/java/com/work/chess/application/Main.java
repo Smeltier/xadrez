b9cad5d01b032f5aad9735a18043f1fbb07777a0
Smeltier <connect.goncalves@gmail.com>
2025-12-08 22:21:59 -0300

    att: atualiza a classe Main.

--- src/main/java/com/work/chess/application/Main.java
+++ src/main/java/com/work/chess/application/Main.java
@@ -1,13 +1,8 @@
 package main.java.com.work.chess.application;
 
-import main.java.com.work.chess.controller.Game;
-import main.java.com.work.chess.util.ConsoleUtils;
-
 public class Main {
     public static void main(String[] args) {
-        ConsoleUtils.defineUTF8();
-        Game chessGame = new Game();
-        System.out.println("Iniciando Chess Game...");
-        chessGame.run();
+        ChessApp app = new ChessApp();
+        app.start();
     }
 }
