67b38833fa708af4eee04ef1ba1cd64729755ca0
Smeltier <connect.goncalves@gmail.com>
2025-12-11 21:59:59 -0300

    att: adiciona os outros movimentos especial na lista de SpecialMovesRegistry.

--- src/main/java/com/work/chess/util/SpecialMovesRegistry.java
+++ src/main/java/com/work/chess/util/SpecialMovesRegistry.java
@@ -3,17 +3,21 @@ package main.java.com.work.chess.util;
 import java.util.ArrayList;
 import java.util.List;
 
-import main.java.com.work.chess.interfaces.ISpecialMoves;
+import main.java.com.work.chess.interfaces.ISpecialMove;
+import main.java.com.work.chess.model.Castling;
+import main.java.com.work.chess.model.EnPassant;
 import main.java.com.work.chess.model.Promotion;
 
 public class SpecialMovesRegistry {
-    private static final ArrayList <ISpecialMoves> specialMoves = new ArrayList<>();   
+    private static final ArrayList <ISpecialMove> specialMoves = new ArrayList<>();   
 
     static {
         specialMoves.add(new Promotion());
+        specialMoves.add(new EnPassant());
+        specialMoves.add(new Castling());
     }
 
-    public static List<ISpecialMoves> getAll() {
+    public static List<ISpecialMove> getAll() {
         return specialMoves;
     }
 }
