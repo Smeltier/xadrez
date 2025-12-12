e7bb01eed0a30af30f8550693bbf81f861d0593f
Smeltier <connect.goncalves@gmail.com>
2025-12-11 22:06:47 -0300

    att: atualiza ordem de verificação dos SM em PieceMover.

--- src/main/java/com/work/chess/controller/PieceMover.java
+++ src/main/java/com/work/chess/controller/PieceMover.java
@@ -2,38 +2,36 @@ package main.java.com.work.chess.controller;
 
 import java.util.List;
 
-import main.java.com.work.chess.interfaces.ISpecialMoves;
+import main.java.com.work.chess.interfaces.ISpecialMove;
 import main.java.com.work.chess.model.Board;
 import main.java.com.work.chess.model.Move;
-import main.java.com.work.chess.model.Promotion;
-import main.java.com.work.chess.model.EnPassant;
-import main.java.com.work.chess.model.Castling;
 
 public class PieceMover {
 
-    private static final List<ISpecialMoves> specials = List.of(
-        new Promotion(),
-        new EnPassant(),
-        new Castling()
-    );
-
     private PieceMover() {}
 
-    public static void move(Board board, Move move) {
-        applyMove(board, move);
-        for (ISpecialMoves s : specials) {
+    public static void move(Board board, Move move, List<ISpecialMove> specials) {
+        ISpecialMove specialMoveToExecute = null;
+        
+        for (ISpecialMove s : specials) {
             if (s.canExecute(board, move)) {
-                s.execute(board, move);
+                specialMoveToExecute = s;
+                break;
             }
+        }
+
+        applyMove(board, move);
+
+        if (specialMoveToExecute != null) {
+            specialMoveToExecute.execute(board, move);
         }
     }   
 
     private static void applyMove(Board board, Move move) {
         try {
-            board.setPieceAt(move.from, null);
-            move.piece.setPosition(move.to);
-            board.setPieceAt(move.to, move.piece);
-            
+            board.setPieceAt(move.getOrigin(), null);
+            move.getPiece().setPosition(move.getDestiny());
+            board.setPieceAt(move.getDestiny(), move.getPiece());
         } catch (Exception e) {
             throw new RuntimeException("Erro crítico ao mover peça no tabuleiro: " + e.getMessage());
         }
