3911be1019a0ddbbf7a5ed4c3ca23fd7747b7363
Smeltier <connect.goncalves@gmail.com>
2025-12-11 22:04:48 -0300

    att: atualiza ChessRules para suportar Move.

--- src/main/java/com/work/chess/controller/ChessRules.java
+++ src/main/java/com/work/chess/controller/ChessRules.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import main.java.com.work.chess.enums.PieceColor;
 import main.java.com.work.chess.model.Board;
+import main.java.com.work.chess.model.Move;
 import main.java.com.work.chess.model.Position;
 import main.java.com.work.chess.model.chess_pieces.King;
 import main.java.com.work.chess.model.chess_pieces.Piece;
@@ -13,105 +14,113 @@ public class ChessRules {
 
     private ChessRules () {}
 
-    public static boolean isMoveLegal (Board board, Piece piece, Position from, Position to) {
-        List <Position> validMoves = piece.getValidMoves (board);
-        
-        boolean isGeometricValid = validMoves.stream ()
-            .anyMatch (p -> p.getRow () == to.getRow () && p.getCol () == to.getCol ());
+    public static boolean isMoveLegal (Board board, Move move) {
+        Piece piece = move.getPiece();
+        Position to = move.getDestiny();
+
+        boolean isGeometricValid = piece.getValidMoves(board).stream()
+            .anyMatch(p -> p.equals(to));
 
         if (!isGeometricValid) {
             return false;
         }
 
-        Piece capturedPiece = board.getPieceAt (to);
+        // Piece capturedPiece = move.getCapturedPiece();
+        Position from = move.getOrigin();
         
-        try {
-            board.setPieceAt (to, piece);
-            board.setPieceAt (from, null);
-            piece.setPosition (to); 
+        board.setPieceAt(to, piece);
+        board.setPieceAt(from, null);
+        piece.setPosition(to);
 
-            if (isKingInCheck (board, piece.getColor ())) {
-                undoMove(board, piece, from, to, capturedPiece);
-                return false;
-            }
+        boolean kingInCheck = isKingInCheck(board, piece.getColor());
 
-            undoMove(board, piece, from, to, capturedPiece);
-        } catch (Exception e) {
-            return false;
-        }
+        undoMove(board, move);
 
-        return true;
-    }
-
-    private static void undoMove(Board board, Piece piece, Position from, Position to, Piece capturedPiece) {
-        try {
-            board.setPieceAt (from, piece);
-            board.setPieceAt (to, capturedPiece);
-            piece.setPosition (from);
-        } catch (Exception e) {}
+        return !kingInCheck;
     }
 
     public static boolean isKingInCheck (Board board, PieceColor kingColor) {
-        Position kingPosition = findKingPosition (board, kingColor);
-        
+        Position kingPosition = findKingPosition(board, kingColor);
+
         if (kingPosition == null) return false;
 
         PieceColor enemyColor = (kingColor == PieceColor.WHITE) ? PieceColor.BLACK : PieceColor.WHITE;
-        List <Piece> enemyPieces = getPiecesByColor (board, enemyColor);
+        List<Piece> enemyPieces = getPiecesByColor(board, enemyColor);
 
         for (Piece enemy : enemyPieces) {
-            List <Position> threats = enemy.getValidMoves (board);
+            List<Position> threats = enemy.getValidMoves(board);
             for (Position threat : threats) {
-                if (threat.equals (kingPosition)) {
+                if (threat.equals(kingPosition)) {
                     return true;
                 }
             }
         }
+
         return false;
     }
 
     public static boolean isCheckMate (Board board, PieceColor kingColor) {
-        if (!isKingInCheck (board, kingColor)) {
+        if (!isKingInCheck(board, kingColor)) {
             return false;
         }
 
-        List <Piece> myPieces = getPiecesByColor (board, kingColor);
+        List<Piece> myPieces = getPiecesByColor(board, kingColor);
 
         for (Piece piece : myPieces) {
-            List <Position> potentialMoves = piece.getValidMoves (board);
-            
+            List<Position> potentialMoves = piece.getValidMoves(board);
+
             for (Position dest : potentialMoves) {
-                if (isMoveLegal (board, piece, piece.getPosition (), dest)) {
+                Piece capturedContent = board.getPieceAt(dest);
+                Move emulationMove = new Move(piece, piece.getPosition(), dest, capturedContent);
+
+                if (isMoveLegal(board, emulationMove)) {
                     return false;
                 }
             }
         }
-
+        
         return true;
+    }
+
+    private static void undoMove (Board board, Move move) {
+        Position origin = move.getOrigin();
+        Position destiny = move.getDestiny();
+        Piece piece = move.getPiece();
+        Piece captured = move.getCapturedPiece();
+
+        board.setPieceAt(origin, piece);
+        board.setPieceAt(destiny, captured);
+        piece.setPosition(origin);
     }
 
     private static Position findKingPosition (Board board, PieceColor color) {
         for (int i = 1; i <= 8; i++) {
             for (int j = 1; j <= 8; j++) {
-                Piece p = board.getPieceAt (new Position (i, j));
-                if (p instanceof King && p.getColor () == color) {
-                    return p.getPosition ();
+                Position pos = new Position(i, j);
+                Piece p = board.getPieceAt(pos);
+
+                if (p instanceof King && p.getColor() == color) {
+                    return pos;
                 }
             }
         }
+
         return null;
     }
 
-    private static List <Piece> getPiecesByColor (Board board, PieceColor color) {
-        List <Piece> pieces = new ArrayList <> ();
+    private static List<Piece> getPiecesByColor (Board board, PieceColor color) {
+        List<Piece> pieces = new ArrayList<>();
+
         for (int i = 1; i <= 8; i++) {
             for (int j = 1; j <= 8; j++) {
-                Piece p = board.getPieceAt (new Position (i, j));
-                if (p != null && p.getColor () == color) {
-                    pieces.add (p);
+                Piece p = board.getPieceAt(new Position(i, j));
+
+                if (p != null && p.getColor() == color) {
+                    pieces.add(p);
                 }
             }
         }
+        
         return pieces;
     }
 }
