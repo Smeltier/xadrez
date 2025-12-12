cb1be9dd0eceb048afa8dc4ebdf9d17376754c7c
Smeltier <connect.goncalves@gmail.com>
2025-12-11 22:03:27 -0300

    add:

---
+++ src/main/java/com/work/chess/interfaces/ISpecialMove.java
@@ -0,0 +1,9 @@
+package main.java.com.work.chess.interfaces;
+
+import main.java.com.work.chess.model.Board;
+import main.java.com.work.chess.model.Move;
+
+public interface ISpecialMove {
+    boolean canExecute(Board board, Move move);
+    void execute(Board board, Move move);
+}
