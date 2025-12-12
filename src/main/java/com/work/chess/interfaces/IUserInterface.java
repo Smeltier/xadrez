1db5b38ae8b903553ddeb90fcc7a03e45111d904
Smeltier <connect.goncalves@gmail.com>
2025-12-11 21:59:13 -0300

    add: adiciona a interface base para interação com o usuário.

---
+++ src/main/java/com/work/chess/interfaces/IUserInterface.java
@@ -0,0 +1,13 @@
+package main.java.com.work.chess.interfaces;
+
+import main.java.com.work.chess.model.Board;
+import main.java.com.work.chess.model.Position;
+
+public interface IUserInterface {
+    void showBoard (Board board);
+    void showMessage (String message);
+    void showError (String error);
+    String readInput (String prompt);
+    Position[] readMove ();
+    void close ();
+}
