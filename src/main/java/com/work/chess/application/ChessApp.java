ba3516db6bcea34d30bcc380cd09f454edbab898
Smeltier <connect.goncalves@gmail.com>
2025-12-11 22:04:14 -0300

    att: adiciona UserInterface em ChessApp.

--- src/main/java/com/work/chess/application/ChessApp.java
+++ src/main/java/com/work/chess/application/ChessApp.java
@@ -1,36 +1,40 @@
 package main.java.com.work.chess.application;
 
-import java.util.Scanner;
 import main.java.com.work.chess.controller.Game;
+import main.java.com.work.chess.interfaces.IUserInterface;
+import main.java.com.work.chess.view.ConsoleUI;
 import main.java.com.work.chess.view.MenuView;
 
 public class ChessApp {
 
-    public void start() {
-        Scanner sc = new Scanner(System.in);
+    public void start () {
+        IUserInterface ui = new ConsoleUI();
         boolean inMenu = true;
+
+        MenuView.setUI(ui);
 
         while (inMenu) {
             MenuView.startMenu();
-            String option = sc.nextLine().trim();
+            String option = ui.readInput("");
 
             switch (option) {
                 case "1" -> {
-                    Game game = new Game();
+                    Game game = new Game(ui);
                     game.run(); 
                 }
                 case "2" -> {
                     MenuView.showRules();
-                    System.out.println("Pressione ENTER para voltar...");
-                    sc.nextLine();
+                    ui.readInput("Pressione ENTER para voltar...");
                 }
                 case "3" -> {
-                    System.out.println("Saindo...");
+                    ui.showMessage("Saindo...");
                     inMenu = false;
                 }
-                default -> System.out.println("Opção inválida.");
+                
+                default -> ui.showError("Opção inválida.");
             }
         }
-        sc.close();
+
+        ui.close();
     }
 }
